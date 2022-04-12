5048fb45c9779ff9f2c1f7bdf1ee41d9d095b983
rhithulroy <97996262+rhithulroy@users.noreply.github.com>
2022-04-12 15:13:59 -0400

    Create Lab3Final.ps1

---
+++ powershell/Lab3Final.ps1
@@ -0,0 +1,3 @@
+﻿Get-Ciminstance -ClassNa win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true" } | 
+Format-Table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
